a3953163108fa1dafeb210eb3f8485276d518378
vanmer <60948275+vanmer@users.noreply.github.com>
2020-10-27 19:54:09 +0100

    add css and delay function

---
+++ docs/js/main.js
@@ -0,0 +1,13 @@
+function delay(n) {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    });
+}
+
+function pageTransition() {
+    var tl = gsap.timeline();
+    
+}
